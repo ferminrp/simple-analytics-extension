64de1429ca180e695b69ecf5815e205961f3efef
ferminrp <frodriguezpenelas@gmail.com>
2019-08-16 07:53:39 -0300

    Arreglada despues del a puteada

--- background.js
+++ background.js
@@ -4,31 +4,41 @@ const url_donarg = 'https://simpleanalytics.com/donarg.com.json';
 
 var total_pageviews = 0
 
-function abbrNum(r,t){t=Math.pow(10,t);for(var a=["k","m","b","t"],n=a.length-1;n>=0;n--){var e=Math.pow(10,3*(n+1));if(e<=r){1e3==(r=Math.round(r*t/e)/t)&&n<a.length-1&&(r=1,n++),r+=a[n];break}}return r};
+function abbrNum(r, t) {
+    t = Math.pow(10, t);
+    for (var a = ["k", "m", "b", "t"], n = a.length - 1; n >= 0; n--) {
+        var e = Math.pow(10, 3 * (n + 1));
+        if (e <= r) {
+            1e3 == (r = Math.round(r * t / e) / t) && n < a.length - 1 && (r = 1, n++), r += a[n];
+            break
+        }
+    }
+    return r
+};
 
-chrome.tabs.onUpdated.addListener(function(tabId,changeInfo,tab) {
-  fetch(url_openstartups)
-    .then(response => response.json())
-    .then(data => {
-      var new_pageviews = data.pageviews;
+
+    chrome.tabs.onCreated.addListener(function(tabId, changeInfo, tab) {
       total_pageviews = 0;
-      total_pageviews = total_pageviews + new_pageviews;
-    })
-    .catch(console.error);
-    fetch(url_influencers)
-      .then(response => response.json())
-      .then(data => {
-        var new_pageviews = data.pageviews;
-        total_pageviews = total_pageviews + new_pageviews;
-      })
-      .catch(console.error);
-      fetch(url_donarg)
-        .then(response => response.json())
-        .then(data => {
-          var new_pageviews = data.pageviews;
-          total_pageviews = total_pageviews + new_pageviews;
-        })
-        .catch(console.error);
-        var number = abbrNum(total_pageviews,1)
-        chrome.browserAction.setBadgeText({text:number.toString()});
-});
+      $.getJSON(url_openstartups, function(data) {
+          //data is the JSON string
+          console.log(data.pageviews);
+          var pageviews = data.pageviews;
+          total_pageviews += pageviews;
+          chrome.browserAction.setBadgeText({text: abbrNum(total_pageviews,1).toString()});
+      });
+      $.getJSON(url_influencers, function(data) {
+          //data is the JSON string
+          console.log(data.pageviews);
+          var pageviews = data.pageviews;
+          total_pageviews += pageviews;
+          chrome.browserAction.setBadgeText({text: abbrNum(total_pageviews,1).toString()});
+      });
+      $.getJSON(url_donarg, function(data) {
+          //data is the JSON string
+          console.log(data.pageviews);
+          var pageviews = data.pageviews;
+          total_pageviews += pageviews;
+          chrome.browserAction.setBadgeText({text: abbrNum(total_pageviews,1).toString()});
+      });
+        chrome.browserAction.setBadgeText({text: abbrNum(total_pageviews,1).toString()});
+    });
